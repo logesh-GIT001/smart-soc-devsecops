2e540a618703d0e92c808b1d66d40ccf1c99364a
Logeshwaran S <logesh.spy@gmail.com>
2026-06-10 12:06:45 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -80,4 +80,4 @@ def triage_traffic(sample: TrafficSample):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)  # nosec B104
